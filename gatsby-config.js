805a8e497ff9ccda4b202db52329ef07ac54633d
Haroen Viaene <hello@haroen.me>
2018-09-13 14:02:36 -0700

    fix: use styled components plugin
    Also removed yarn.lock since you seem to be using npm

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `KMbappe Mobile App by Antoine Plu`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet'
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components'
   ],
 }
